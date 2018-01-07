eb33d7c90d0a54b62ae56d13782c1af51b9be599
kinseyost <kinsey.vanost@gmail.com>
2018-01-07 01:21:48 +0000

    Adds access-control-allow-origin and headers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const app = express();
 const port = 8081;
 const isRunningInDocker = process.env.DOCKER_ENV;
 const url = isRunningInDocker ? 'http://storage:8082' : 'https://localhost:8082';
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use('/uploads.json', proxy({ target: url, changeOrigin: true }));
+
 app.listen(port, () => console.log('connected to port', port));
